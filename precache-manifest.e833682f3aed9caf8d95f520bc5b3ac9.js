ab7f3f152e414272652f7d566b64109fd1ec205a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-25 12:08:58 +0000

    Deploy phatbhoy67/drum-machine to github.com/phatbhoy67/drum-machine.git:gh-pages

---
+++ precache-manifest.e833682f3aed9caf8d95f520bc5b3ac9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa0f9e5cc48d272e02e",
+    "url": "/drum-machine/static/css/main.82c9abc4.chunk.css"
+  },
+  {
+    "revision": "8fa0f9e5cc48d272e02e",
+    "url": "/drum-machine/static/js/main.8fa0f9e5.chunk.js"
+  },
+  {
+    "revision": "c73e5ac2cc3e61c10a29",
+    "url": "/drum-machine/static/js/runtime~main.c73e5ac2.js"
+  },
+  {
+    "revision": "0e874caa4a2c64f66f5a",
+    "url": "/drum-machine/static/js/2.0e874caa.chunk.js"
+  },
+  {
+    "revision": "f34eaf3443ebeceedbf6adbfc903e8eb",
+    "url": "/drum-machine/static/media/Electronic-Tom-1.f34eaf34.wav"
+  },
+  {
+    "revision": "346af2586fa993cf57956f6c32b8da5f",
+    "url": "/drum-machine/static/media/Alesis-S4-Plus-FatSynSlap-C2.346af258.wav"
+  },
+  {
+    "revision": "40f42a70f3d5834aa5bdca4266b1804d",
+    "url": "/drum-machine/static/media/Bass-Drum-1.40f42a70.wav"
+  },
+  {
+    "revision": "8f9aca6ca4cfe9948af3917bf46450a3",
+    "url": "/drum-machine/static/media/Casio-VZ-10M-Fretless-Bass-C2.8f9aca6c.wav"
+  },
+  {
+    "revision": "4121f204e9b85cf512264cdf465317e7",
+    "url": "/drum-machine/static/media/Cowbell-1.4121f204.wav"
+  },
+  {
+    "revision": "03181901e6809a688ff2b29c4b620b3d",
+    "url": "/drum-machine/static/media/Korg-N1R-High-Q.03181901.wav"
+  },
+  {
+    "revision": "afc103f41db6a4061c253dc0ba05b815",
+    "url": "/drum-machine/static/media/Korg-N1R-Open-Surdo.afc103f4.wav"
+  },
+  {
+    "revision": "b59ebe0f6fe00a2b89dd924229abd815",
+    "url": "/drum-machine/static/media/Korg-N1R-Pedal-Hi-Hat.b59ebe0f.wav"
+  },
+  {
+    "revision": "14f55b7cd559db7df0eedb0fc639786f",
+    "url": "/drum-machine/static/media/Korg-N1R-Vibraslap.14f55b7c.wav"
+  },
+  {
+    "revision": "7fa262c1bb1d1134bf55ac921683c5d2",
+    "url": "/drum-machine/index.html"
+  }
+];
